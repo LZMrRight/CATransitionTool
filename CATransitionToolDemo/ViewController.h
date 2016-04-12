fa7cb46f886017995ce8a8196a6957bcbbde3c52
LZMrRight <275068651@qq.com>
2016-04-12 14:16:49 +0800

    Initial Commit

---
+++ CATransitionToolDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CATransitionToolDemo
+//
+//  Created by Mr.Right on 16/4/12.
+//  Copyright © 2016年 lizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CATransitionToolDemoTests/CATransitionToolDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  CATransitionToolDemoTests.m
+//  CATransitionToolDemoTests
+//
+//  Created by Mr.Right on 16/4/12.
+//  Copyright © 2016年 lizheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CATransitionToolDemoTests : XCTestCase
+
+@end
+
+@implementation CATransitionToolDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
